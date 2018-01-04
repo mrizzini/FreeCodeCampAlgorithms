0edc03ee0f1373bed211c327a1f23b8f5a8d1f6d
Matthew Rizzini <matthewrizzini@gmail.com>
2018-01-04 21:52:28 +0000

    continued working on algorithm

---
+++ pigLatin/index.js
@@ -0,0 +1,26 @@
+// needs more work
+
+
+
+function translatePigLatin(str) {
+  
+  str = str.split("");
+  
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u") {
+      str.push("way");
+      break;
+    } else if (str[i] !== "a" || str[i] !== "e" || str[i] !== "i" || str[i] !== "o" || str[i] !== "u"){
+       str.push(str[i]);
+      str.push('ay');
+      str.slice(0, 1);
+      break;
+    }
+  }
+  
+ str =  str.join("");
+  
+  return str;
+}
+
+translatePigLatin("glove");
