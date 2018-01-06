1d8cafc491204c724a3c63324585147d7cb4d070
Matthew Rizzini <matthewrizzini@gmail.com>
2018-01-06 03:56:06 +0000

    Translate the provided string to pig latin.

--- pigLatin/index.js
+++ pigLatin/index.js
@@ -1,27 +1,32 @@
-// needs more work
-
-
+// needs some serious cleaning
 
 function translatePigLatin(str) {
-  
-  str = str.split("");
-  
-  for (var i = 0; i < str.length; i++) {
-    if (str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u") {
-      str.push("way");
-      break;
-    } else if (str[i] !== "a" || str[i] !== "e" || str[i] !== "i" || str[i] !== "o" || str[i] !== "u"){
-       str.push(str[i]);
-      str.push('ay');
-      str.slice(0, 1);
-      break;
-    }
-  }
-  
- str =  str.join("");
-  
-  return str;
+	var pigLatin = "";
+	var vowels = ["a", "e", "i", "o", "u"];
+	var consonants = [];
+	if (str[0] === vowels[0] || str[0] === vowels[1] || str[0] === vowels[3] || str[0] === vowels[4]) {
+		pigLatin = str + "way";
+		return pigLatin;
+	} else {
+		str = str.split('');
+		consonants.push(str[0]);
+		str.splice(0, 1);
+		str = str.join('');
+		pigLatin = str + consonants + "ay";
+		if (str[0] === vowels[0] || str[0] === vowels[1] || str[0] === vowels[3] || str[0] === vowels[4]) {
+			return pigLatin;
+		}
+	}
+	if (str[0] !== "a" || str[0] !== "e" || str[0] !== "i" || str[0] !== "o" || str[0] !== "u") {
+		str = str.split('');
+		consonants.push(str[0]);
+		str.splice(0, 1);
+		str = str.join('');
+		consonants = consonants.join("");
+		return str + consonants + "ay";
+	}
+	return pigLatin;
 }
-
 translatePigLatin("glove");
+
 
