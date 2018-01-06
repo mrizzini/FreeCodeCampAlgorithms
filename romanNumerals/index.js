97f96f1921f81e6096da5283b15da31bcb9d0548
Matthew Rizzini <matthewrizzini@gmail.com>
2018-01-06 15:54:26 +0000

    starting with framing everything

---
+++ romanNumerals/index.js
@@ -0,0 +1,13 @@
+// framing algorithm
+
+function convertToRoman(num) {
+  var digits = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+  var romans = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+  var romanNumeral = [];
+  
+
+  
+ return num;
+}
+
+convertToRoman(36);
