0912f0722a4047b7eb06ec4b832d06eded767898
Matthew Rizzini <matthewrizzini@gmail.com>
2018-01-11 15:32:16 +0000

    added comments

--- sumAllOddFibonacciNumbers/index.js
+++ sumAllOddFibonacciNumbers/index.js
@@ -1,16 +1,16 @@
 function sumFibs(num) {
-	var numFib = [1, 1];
-	var total = 0;
-	for (var e = 0; numFib.slice(-1)[0] <= num; e++) {
-		numFib.push((numFib[e] + numFib[e + 1]));
+	var numFib = [1, 1]; // sets the first two Fib numbers
+	var total = 0; // sets total variable
+	for (var e = 0; numFib.slice(-1)[0] <= num; e++) { // sets loop that stops when last numFib is greater than num
+		numFib.push((numFib[e] + numFib[e + 1])); //pushes sum of the previous two number to numFib
 	}
-	numFib.pop();
-	for (var i = 0; i < numFib.length; i++) {
+	numFib.pop(); // deletes last numFib. For some reason my loop goes one further than it should
+	for (var i = 0; i < numFib.length; i++) { //loops thru completed numFib and adds only the odd numbers
 		if (numFib[i] % 2 !== 0) {
-			total += numFib[i];
+			total += numFib[i]; // sums all odd numbers and stores in total
 		}
 	}
 	// return numFib;
 	return total;
 }
-sumFibs(10);
+sumFibs(30423);
