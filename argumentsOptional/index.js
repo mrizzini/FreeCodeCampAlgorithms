8e1d04a94142cefdddef8eaa8eeb17d8300263f7
Matthew Rizzini <matthewrizzini@gmail.com>
2018-05-23 01:18:53 +0000

    added a few solutions for arguments optional

--- argumentsOptional/index.js
+++ argumentsOptional/index.js
@@ -22,3 +22,50 @@
 // // }
 
 // // console.log(x);
+
+
+function addTogether() {
+    var numOne = arguments[0];
+    if (typeof(numOne) !== 'number') {
+        return undefined;
+    }
+    if (arguments.length === 2) {
+        if (typeof(arguments[1]) === 'number') {
+            return numOne + arguments[1];
+        }
+        return undefined;
+    }
+    
+    return function() {
+        if (typeof(arguments[0]) === 'number') {
+            return numOne + arguments[0];
+        }
+        return undefined;
+    };
+}
+
+addTogether(2,3);
+
+// if 2 arguments add them together
+// must be numbers
+// if 1 argument send function back to get the second item
+
+// OR TIERNARY FUNCTION: 
+
+function addTogether() {
+  
+    var numOne = arguments[0];
+    if (typeof(numOne) !== 'number') {
+        return undefined;
+    }
+  
+    if (arguments.length === 2) {
+      return typeof(arguments[1]) === 'number' ? numOne + arguments[1] :         undefined;    
+    }
+  
+    return function() {
+        return typeof(arguments[0]) === 'number' ? numOne + arguments[0] : undefined;
+    };
+}
+
+addTogether(2,3);
