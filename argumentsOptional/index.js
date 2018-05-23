4fc996e0a72780a416933a3af489bef8697febb3
Matthew Rizzini <matthewrizzini@gmail.com>
2018-05-23 00:59:00 +0000

    added second solution for binary agent

--- argumentsOptional/index.js
+++ argumentsOptional/index.js
@@ -1,24 +1,24 @@
-// closures
+// // closures
 
-function one(x){
-    // console.log(x + 2);
-    console.log(x + 2);
-    return function(y){
-        console.log(x + y);
-        // makes x the 2
-        // makes y the 3
-    };
-}
-
-one(2)(3);
-
-
-var two = one(5);
-two(8);
-
-// in memory:
-// function     {
-    // console.log(2 + y);
+// function one(x){
+//     // console.log(x + 2);
+//     console.log(x + 2);
+//     return function(y){
+//         console.log(x + y);
+//         // makes x the 2
+//         // makes y the 3
+//     };
 // }
 
-// console.log(x);
+// one(2)(3);
+
+
+// var two = one(5);
+// two(8);
+
+// // in memory:
+// // function     {
+//     // console.log(2 + y);
+// // }
+
+// // console.log(x);
---
+++ binaryAgents/index2.js
@@ -0,0 +1,19 @@
+
+function binaryAgent(str) {
+    var strArray = str.split(" ");
+    strArray = strArray.map(function(binary) {
+        return parseInt(binary, 2);
+    });
+    str = String.fromCharCode.apply(null, strArray);
+    return str;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+// OR ONE LINE:
+
+// function binaryAgent(str) {
+//     return String.fromCharCode.apply(null, str.split(" ").map(function(binary) {return parseInt(binary, 2);}));
+// }
+
+// binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
