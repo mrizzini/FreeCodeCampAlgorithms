025b46031ba2d2ef39ca02dd17547edba77e4c73
Matthew Rizzini <matthewrizzini@gmail.com>
2018-05-31 19:13:32 +0000

    added comments for slice and splice

--- sliceAndSplice/index.js
+++ sliceAndSplice/index.js
@@ -1,4 +1,7 @@
 function frankenSplice(arr1, arr2, n) {
+    
+    // arr2.slice(0, n) will give you the first part of arr, before the index of n
+    // then .concat adds in arr1, and then the sliced off part of arr2 that was not included in the first slice
 
   var frankenstein = arr2.slice(0, n).concat(arr1, arr2.slice(n));
 
